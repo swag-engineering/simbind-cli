1aa89c454a8d2f89f4ca9e818b144259b1ade988
Anton <anton.rak@swag-engineering.io>
2024-01-31 01:02:50 +0100

    finished setuptools, time to write tests

---
+++ simbind/__init__.py
@@ -0,0 +1,9 @@
+import warnings
+
+warnings.warn(
+    "Simbind package is intended to be used as a standalone tool, not as an importable module. "
+    "If you want to use its functionality programmatically, please refer to "
+    "https://github.com/swag-engineering/simbind-architect and "
+    "https://github.com/swag-engineering/simulink-exporter",
+    UserWarning
+)
--- simbind/__main__.py
+++ simbind/__main__.py
@@ -36,7 +36,7 @@ def prepare_dir(path: str, overwrite: bool):
         delete_dir_internals(path)
 
 
-async def main(
+async def assemble(
         slx_path: str,
         package_name: str,
         exporter_out_dir: str,
@@ -96,51 +96,51 @@ async def main(
         raise RuntimeError("stderr")
 
 
-if __name__ == '__main__':
+async def async_main():
     parser = argparse.ArgumentParser(
-        prog='Simbind CLI',
-        description='Tool to generate Python wheel package from Simulink model.'
+        prog='simbind',
+        description='Tool to generate Python wheel package from Simulink model.',
     )
     parser.add_argument(
         '--slx-path',
         dest='slx_path',
-        help='Path to Simulink .slx file',
+        help='Path to Simulink .slx file.',
         required=True,
         type=os.path.abspath
     )
     parser.add_argument(
         '--pkg-name',
         dest='pkg_name',
-        help='Name of the output Python package',
+        help='Name of the output Python package. (default: \'%(default)s\')',
         type=str,
         default='model'
     )
     parser.add_argument(
         '--wheel-out-dir',
         dest='wheel_out_dir',
-        help='Path to folder where wheel package will be stored',
-        default='./',
+        help='Path to folder where wheel package will be stored. (default: \'%(default)s\')',
+        default='.',
         type=os.path.abspath
     )
     parser.add_argument(
         '--solver',
         dest='solver',
         choices=['ode1', 'ode2', 'ode3', 'ode4', 'ode5'],
-        help='Fixed-step solver',
+        help='Fixed-step solver. (default: \'%(default)s\')',
         type=str,
         default='ode5'
     )
     parser.add_argument(
         '--step',
         dest='step_size',
-        help='Fixed step size',
+        help='Fixed step size. (default: \'%(default)s\')',
         type=float,
         default=0.001
     )
     parser.add_argument(
         '--license-text',
         dest='license_text',
-        help='License text that will be included in output Python wheel package',
+        help='License text that will be included in output Python wheel package. (default: \'%(default)s\')',
         type=str,
         default=""
     )
@@ -151,22 +151,25 @@ if __name__ == '__main__':
         help='Specifies the level of verbosity. Example: -vvv',
         action='count'
     )
+    # [DEBUG] Path to directory that will be used to store .c and .h files exported by Simulink
     parser.add_argument(
         '--exporter-out-dir',
         dest='exporter_out_dir',
-        help='[DEBUG] Path to directory that will be used to store .c and .h files exported by Simulink.',
+        help=argparse.SUPPRESS,
         type=os.path.abspath
     )
+    # [DEBUG] Directory where output Python model will be stored
     parser.add_argument(
         '--models-out-dir',
         dest='models_out_dir',
-        help='[DEBUG] Directory where output Python model will be stored.',
+        help=argparse.SUPPRESS,
         type=os.path.abspath
     )
+    # [DEBUG] Flag to delete models-out-dir and exporter-out-dir folders' content if they are not empty
     parser.add_argument(
         '--overwrite',
         dest='overwrite',
-        help='[DEBUG] Flag to delete models-out-dir and exporter-out-dir folders\' content if they are not empty',
+        help=argparse.SUPPRESS,
         action='store_true'
     )
 
@@ -209,20 +212,26 @@ if __name__ == '__main__':
         else:
             models_out_tmp_dir = tempfile.TemporaryDirectory()
 
-        asyncio.run(
-            main(
-                args.slx_path,
-                args.pkg_name,
-                args.exporter_out_dir or exporter_out_tmp_dir.name,
-                args.models_out_dir or models_out_tmp_dir.name,
-                args.wheel_out_dir,
-                args.solver,
-                args.step_size,
-                args.license_text
-            )
+        await assemble(
+            args.slx_path,
+            args.pkg_name,
+            args.exporter_out_dir or exporter_out_tmp_dir.name,
+            args.models_out_dir or models_out_tmp_dir.name,
+            args.wheel_out_dir,
+            args.solver,
+            args.step_size,
+            args.license_text
         )
     finally:
         if exporter_out_tmp_dir:
             exporter_out_tmp_dir.cleanup()
         if models_out_tmp_dir:
             models_out_tmp_dir.cleanup()
+
+
+def main():
+    asyncio.run(async_main())
+
+
+if __name__ == '__main__':
+    main()
